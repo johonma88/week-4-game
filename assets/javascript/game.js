646affbbb091afe1fdeeee28d51ac60044045a94
Johanna Honma <johonma@hotmail.com>
2017-11-23 10:45:35 -0700

    11/23/17

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+
+
+var characters = [
+    {name: 'darthVader',
+    hp : 10,
+    attackPower: 1,
+    countAttack:1,
+    image: 'darth-vaider.png' },
+
+    {name: 'captainPlasma',
+    hp :10,
+    attackPower : 1,
+    countAttack: 1,
+    image: 'captain_plasma.png'},
+
+    {name: 'han',
+     hp : 10,
+     attackPower : 1,
+    countAttack : 1,
+    image: 'hanSolo.png'},
+
+    {name: 'yoda',
+        hp :10,
+        attackPower : 1,
+       countAttack: 1,
+       image: 'yoda.png'},
+
+    {name: 'chewbacca',
+    hp :10,
+    attackPower : 1,
+    countAttack: 1,
+    image: 'chewbacca.png'}
+]
+   
+
+
+// hpDarth.innerHTML =  "health Power" + characters[0].hp;
+// document.getElementById("hpDarth").innerHTML = "Health Power!" + characters[0].hp;
+
+
+
+// console.log(characters[1].name);
+console.log("health Power "+ characters[0].hp); 
