42386227d81d8a0623d4f790779b42a2c20be6d7
Johanna Honma <johonma@hotmail.com>
2017-11-23 21:21:42 -0700

    nov23

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,59 @@
 
 
 
-var characters = [
-    {name: 'darthVader',
-    hp : 10,
-    attackPower: 1,
-    countAttack:1,
-    image: 'darth-vaider.png' },
+var characters = {
+    darthvader: {
+        name: 'darthVader',
+        hp : 200,
+        attackPower: 1,
+        countAttack:1,
+        status:0,  // defender 0 , attacker1
+        image: 'darth-vaider.png' },
+    
+    captainPlasma:{
+        name: 'captainPlasma',
+        hp :180,
+        attackPower : 1,
+        countAttack: 1,
+        status:0,  // defender 0 , attacker1
+        image: 'captain_plasma.png'},
+   
+    hanSolo: {
+        name: 'hanSolo',
+        hp : 170,
+        attackPower : 1,
+        countAttack : 1,
+        status:0,  // defender 0 , attacker1
+        image: 'hanSolo.png'},
 
-    {name: 'captainPlasma',
-    hp :10,
-    attackPower : 1,
-    countAttack: 1,
-    image: 'captain_plasma.png'},
-
-    {name: 'han',
-     hp : 10,
-     attackPower : 1,
-    countAttack : 1,
-    image: 'hanSolo.png'},
-
-    {name: 'yoda',
-        hp :10,
+    yoda:{
+        name: 'yoda',
+        hp :190,
         attackPower : 1,
        countAttack: 1,
+       status:0,  // defender 0 , attacker1
        image: 'yoda.png'},
 
-    {name: 'chewbacca',
-    hp :10,
-    attackPower : 1,
-    countAttack: 1,
-    image: 'chewbacca.png'}
-]
-   
+    chewbacca:{
+        name: 'chewbacca',
+        hp :170,
+        attackPower : 1,
+        countAttack: 1,
+        status:0,  // defender 0 , attacker1
+        image: 'chewbacca.png'}
+    }
+
+var character = "";
+playerstatus=0; //0 empty  1 full
 
 
-// hpDarth.innerHTML =  "health Power" + characters[0].hp;
-// document.getElementById("hpDarth").innerHTML = "Health Power!" + characters[0].hp;
+console.log("health Power "+ characters.darthvader.hp); 
+
+// $(".colCharacater").click(function(){
+//     // newCharacter= (".this").append;
+//     $(this).hide(); 
+  
+// });
 
 
 
-// console.log(characters[1].name);
-console.log("health Power "+ characters[0].hp); 
