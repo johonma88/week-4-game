88998ccbbfd96d53c30dd367e13b13b1786a53ea
Johanna Honma <johonma@hotmail.com>
2017-11-25 21:39:22 -0700

    last changes 11/25

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,14 @@ var characters = {
     darthvader: {
         name: 'darthVader',
         hp : 200,
-        attackPower: 20,
+        attackPower:20,
         countAttack:1,
         status:0,  // defender 0 , attacker1
         image: 'darth-vaider.png' },
     
     captainPlasma:{
         name: 'captainPlasma',
-        hp :180,
+        hp :140,
         attackPower : 11,
         countAttack: 1,
         status:0,  // defender 0 , attacker1
@@ -20,8 +20,8 @@ var characters = {
    
     hanSolo: {
         name: 'hanSolo',
-        hp : 170,
-        attackPower : 10,
+        hp : 150,
+        attackPower : 9,
         countAttack : 1,
         status:0,  // defender 0 , attacker1
         image: 'hanSolo.png'},
@@ -29,24 +29,32 @@ var characters = {
     yoda:{
         name: 'yoda',
         hp :190,
-        attackPower : 19,
+        attackPower : 18,
        countAttack: 1,
        status:0,  // defender 0 , attacker1
        image: 'yoda.png'},
 
     chewbacca:{
         name: 'chewbacca',
-        hp :170,
-        attackPower : 9,
+        hp :130,
+        attackPower : 7,
         countAttack: 1,
         status:0,  // defender 0 , attacker1
         image: 'chewbacca.png'}
     }
 
 var character = "";
-playerstatus=0; //0 empty  1 full
+var playerstatus=0; //0 empty  1 full
 var hpPlayer =0;
+var attackResult=0;
 var hpEnemy =0;
+var attPlayer=0;
+var attEnemy=0;
+var hpPlayerResult=0;
+var hpEnemyResult=0;
+var playerName="";
+var enemyName="";
+
 
 
 console.log("health Power "+ characters.darthvader.hp); 
